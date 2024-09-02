b26a2d09620e46de287e8ab5bdc6661cffd5aac4
SBuransky <506073@mail.muni.cz>
2024-09-02 14:19:10 +0200

    a

---
+++ lombscargle_periodogram/fourier_transform.py
@@ -0,0 +1,25 @@
+import numpy as np
+from astropy.timeseries import LombScargle
+from matplotlib import pyplot as plt
+
+
+def find_local_maxima(x, y):
+    y = np.array(y)
+    local_maxima_indices = np.where((y[:-2] < y[1:-1]) & (y[1:-1] > y[2:]))[0] + 1
+
+    local_maxima_x = np.array(x)[local_maxima_indices]
+    local_maxima_y = y[local_maxima_indices]
+
+    return local_maxima_x, local_maxima_y
+
+
+def fourier_transform(t, y, frequency, dev=None):
+    if dev is not None:
+        dev = np.abs(dev)
+    ls = LombScargle(t=t, y=y, dy=dev)
+
+    power = ls.power(frequency)
+
+    maxima_x, maxima_y = find_local_maxima(frequency, power)
+    periodogram = frequency, power
+    return periodogram, (maxima_x, maxima_y)
---
+++ service/ga_service.py
@@ -0,0 +1,73 @@
+import os
+from genetic_algorithm.run.one_run import run_genetic_algorithm
+import matplotlib.pyplot as plt
+from utils.fourier_series_value import double_fourier_sequence
+from datetime import datetime as dt
+import numpy as np
+
+
+def tumbler_genetic_algorithm_fit(data,
+                                  fitness_function,
+                                  name,
+                                  m_=1,
+                                  population_size=500,
+                                  gene_range=((-0.2, 0.2), (0.85, 1.15), (0.5, 1.5), (0.5, 1.5)),
+                                  num_generations=100,
+                                  elitism=2,
+                                  crossover_rate=0.95,
+                                  mutation_rate=0.01,
+                                  mutation_range=0.5):
+    # Ensure directories exist
+    os.makedirs('Results/genetic_algorithm/graphs/', exist_ok=True)
+    os.makedirs('Results/genetic_algorithm/fitness/', exist_ok=True)
+    os.makedirs('Results/genetic_algorithm/results/', exist_ok=True)
+
+    final_generation = run_genetic_algorithm(
+        population_size=population_size,
+        fitness_function=fitness_function,
+        num_genes=2 * m_ + 2 * m_ * (2 * m_ + 1) + 3,
+        gene_range=(2 * m_ + 2 * m_ * (2 * m_ + 1)) * (gene_range[0],) + gene_range[1:],
+        num_generations=num_generations,
+        elitism=elitism,
+        crossover_rate=crossover_rate,
+        mutation_rate=mutation_rate,
+        mutation_range=mutation_range
+    )
+    ts = dt.now()
+    ending = str(ts.year)[2:] + '{:02d}'.format(ts.month) + '{:02d}'.format(ts.day) + '-' + '{:02d}'.format(
+        ts.hour) + '{:02d}'.format(ts.minute) + '{:02d}'.format(ts.second)
+
+    days = data['julian_day'].values
+    plt.plot(days, double_fourier_sequence(final_generation[0], m_, days), label='Last Generation Best')
+    plt.plot(days, double_fourier_sequence(final_generation[4], m_, days), label='Overall Best')
+
+    plt.scatter(days, data['noisy_flux'].values, c='gray', marker='+', s=5)
+    plt.errorbar(days, data['noisy_flux'].values, yerr=data['deviation_used'].values, fmt='none', color='black',
+                 elinewidth=1.5, capsize=0)
+
+    plt.xlabel('Time [days]')
+    plt.ylabel('Normalized light flux')
+    plt.title('Genetic Algorithm Fit')
+    plt.legend()
+
+    plt.savefig(f'Results/genetic_algorithm/graphs/{name}_graph_{ending}.pdf')
+    plt.show()
+    plt.close()
+
+    # Plot the fitness over generations
+    plt.plot(np.arange(len(final_generation[2])), final_generation[2])
+    plt.xlabel('Generation')
+    plt.ylabel('Best Fitness')
+    plt.title('Fitness Over Generations')
+    plt.savefig(f'Results/genetic_algorithm/fitness/{name}_fitness_{ending}.pdf')
+    plt.close()
+
+    with open(f'Results/genetic_algorithm/results/{name}_result_{ending}.txt', 'w') as file:
+        file.write('Best in last gen:\n')
+        file.write(str(final_generation[0]))
+        file.write('\nBest fitness in last gen:\n')
+        file.write(str(final_generation[5]))
+        file.write('\n\nBest in all:\n')
+        file.write(str(final_generation[4]))
+        file.write('\nBest fitness in all:\n')
+        file.write(str(final_generation[6]))
