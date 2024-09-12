ed8ccd6a348330ea28cfbcccc4876beb323d50bb
SBuransky <506073@mail.muni.cz>
2024-09-12 17:53:08 +0200

    pre-commit

--- genetic_algorithm/core/fitness_evaluation.py
+++ genetic_algorithm/core/fitness_evaluation.py
@@ -17,23 +17,3 @@ def evaluate_population(population: List[np.ndarray],
     """
     fitness_results = [(individual, fitness_function(individual)) for individual in population]
     return fitness_results
-
-
-'''
-def evaluate_population(population: np.ndarray,
-                        fitness_function: Callable,
-                        n_jobs: int = -1) -> List[Tuple[np.ndarray, float]]:
-    """
-    Evaluate the fitness of the entire population in parallel.
-
-    Parameters:
-    - population: NumPy array of individuals in the population.
-    - fitness_function: Callable function that calculates the fitness of an individual.
-    - n_jobs: Number of CPU cores to use (-1 uses all cores).
-
-    Returns:
-    - List of tuples, each containing an individual and its corresponding fitness value.
-    """
-    fitness_results = Parallel(n_jobs=n_jobs)(delayed(lambda ind: (ind, fitness_function(ind)))(individual)
-                                              for individual in population)
-    return fitness_results'''
