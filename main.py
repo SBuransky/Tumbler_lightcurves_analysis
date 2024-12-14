214b7634643b03ceeac963736bb86d0406ec66ba
Samuel Buransky <sburansky@crab.physics.muni.cz>
2024-12-14 15:35:06 +0100

    test_crab

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
             # 2.020434781036017036e+00  2.677347356997863189e-03    0.494942974
             # 2.745719061407920325e+00  3.487168691736520620e-03    0.364203321
             name=name,
-            num_generations=1000000,
+            num_generations=5,
             elitism=2,
             mutation_rate=0.007,
             mutation_range=0.1,
