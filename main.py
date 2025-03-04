d9893222acd9a6b2ade0a2baabcfb47b426ff675
Samuel Buransky <sburansky@crab.physics.muni.cz>
2025-03-04 20:45:21 +0100

    ppp

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     # Load data (common to both parts)
     # For use follow these instructions:
 
-    name = "ID1916_001"  # Set the name of your data file
+    name = "ID1917_001"  # Set the name of your data file
 
     data = load_data(
         name,
@@ -39,7 +39,7 @@ if __name__ == "__main__":
             "sigma",
             "deviation_used",
         ),
-        appendix=".txt",  # Set the appendix of your data file
+        appendix=".flux",  # Set the appendix of your data file
     )
     data["julian_day"] -= min(data["julian_day"])
     print(len(data["julian_day"]))
@@ -93,7 +93,7 @@ if __name__ == "__main__":
                 + [(-0.04, 0.04)] * (m_ * (2 * m_ + 1))
                 + [(-0.04, 0.04)] * m_
                 + [(-0.04, 0.04)] * m_
-                + [(0.98, 1.02), (-0.00001, 0.00001), (0.56, 0.60), (0.21, 0.25)]
+                + [(0.98, 1.02), (-0.00001, 0.00001), (0.53, 0.57), (0.17, 0.21)]
             ),
             name=name,
             num_generations=20000,
