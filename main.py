bd9e1fc743618b7a55db14528c3fee94e1587940
Samuel Buransky <sburansky@crab.physics.muni.cz>
2025-03-08 16:46:19 +0100

    18

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     # Load data (common to both parts)
     # For use follow these instructions:
 
-    name = "ID1916_007"  # Set the name of your data file
+    name = "ID1918_003"  # Set the name of your data file
 
     data = load_data(
         name,
@@ -39,7 +39,7 @@ if __name__ == "__main__":
             "sigma",
             "deviation_used",
         ),
-        appendix=".txt",  # Set the appendix of your data file
+        appendix=".flux",  # Set the appendix of your data file
     )
     data["julian_day"] -= min(data["julian_day"])
     print(len(data["julian_day"]))
@@ -93,7 +93,7 @@ if __name__ == "__main__":
                 + [(-0.04, 0.04)] * (m_ * (2 * m_ + 1))
                 + [(-0.04, 0.04)] * m_
                 + [(-0.04, 0.04)] * m_
-                + [(0.98, 1.02), (-0.00001, 0.00001), (0.33, 0.37), (0.21, 0.25)]
+                + [(0.98, 1.02), (-0.00001, 0.00001), (0.27, 0.31), (0.32, 0.36)]
             ),
             name=name,
             num_generations=20000,
